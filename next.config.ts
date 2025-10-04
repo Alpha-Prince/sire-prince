947afef3e2b9c223ff2f8bb38a665e90ed1f3b6e
Sire Prince <palpha824@gmail.com>
2025-10-04 12:29:25 +0000

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   output: 'export',
+  basePath: '/sire-prince',
 };
 
 export default nextConfig;
